7119563edb6caa9e18c84d437dfdf7bb2e95e7c6
Ben Wallace <benfwalla@gmail.com>
2023-08-12 19:10:53 -0600

    Improved data entry performance and visuals

--- helper.py
+++ helper.py
@@ -1,3 +1,6 @@
+import streamlit as st
+
+
 def get_ellipse_coords(point: tuple[int, int]) -> tuple[int, int, int, int]:
     center = point
     radius = 10
@@ -7,3 +10,21 @@ def get_ellipse_coords(point: tuple[int, int]) -> tuple[int, int, int, int]:
         center[0] + radius,
         center[1] + radius,
     )
+
+def flip_court(current_court):
+    print('fliing court')
+    if current_court == "img/bball_court_north.png":
+        st.session_state['current_court'] = "img/bball_court_south.png"
+    else:
+        st.session_state['current_court'] = "img/bball_court_north.png"
+
+
+def flip_coordinates(image_size, original_coords):
+    center_x = image_size[0] / 2
+    center_y = image_size[1] / 2
+
+    # Calculate the new flipped coordinates
+    flipped_x = center_x - (original_coords[0] - center_x)
+    flipped_y = center_y - (original_coords[1] - center_y)
+
+    return flipped_x, flipped_y
--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import streamlit as st
 from streamlit_pills import pills
 import pandas as pd
 from PIL import Image, ImageDraw
-from helper import get_ellipse_coords
+from helper import get_ellipse_coords, flip_court, flip_coordinates
 
 from streamlit_image_coordinates import streamlit_image_coordinates
 
 st.set_page_config(
     page_title="Shot + Rebound Location Tracker",
     page_icon="🏀",
-    layout="wide",
+    layout="wide"
 )
 
 "# :basketball: Shot & Rebound Location Tracker"
@@ -24,37 +24,46 @@ if 'data' not in st.session_state:
     data = pd.DataFrame(columns=['shot_x', 'shot_y', 'rebound_x', 'rebound_y'])
     st.session_state['data'] = data
 
+if 'old_value' not in st.session_state:
+    st.session_state['old_value'] = None
+
+if 'current_court' not in st.session_state:
+    st.session_state['current_court'] = "img/bball_court_north.png"
+
 data = st.session_state['data']
 
 selected = pills("Shot or Rebound", ["Shot", "Rebound"])
 
-with Image.open("court.jpg") as img:
+# if st.button('🔄'):
+#     flip_court(st.session_state['current_court'])
+
+# bball_court_north.png is 765×722
+with Image.open(st.session_state['current_court']) as img:
     draw = ImageDraw.Draw(img)
 
     if st.session_state['shot_location']:
         coords = get_ellipse_coords(st.session_state['shot_location'])
         draw.ellipse(coords, fill="red")
-        draw.text((coords[0] + 25, coords[1]),
+        draw.text((coords[0], coords[1] + 25),
                   '({}, {})'.format(st.session_state['shot_location'][0], st.session_state['shot_location'][1]),
-                  fill="red")
+                  fill="black")
     if st.session_state['rebound_location']:
         coords = get_ellipse_coords(st.session_state['rebound_location'])
-        draw.ellipse(coords, fill="yellow")
-        draw.text((coords[0] + 25, coords[1]),
+        draw.ellipse(coords, fill="blue")
+        draw.text((coords[0], coords[1] + 25),
                   '({}, {})'.format(st.session_state['rebound_location'][0], st.session_state['rebound_location'][1]),
-                  fill="red")
+                  fill="black")
 
-    value = streamlit_image_coordinates(img)
+    value = streamlit_image_coordinates(img, key='pil')
 
-    if value is not None:
+    if value != st.session_state['old_value']:
+        st.session_state['old_value'] = value
         point = value["x"], value["y"]
 
         if point != st.session_state["shot_location"] and point != st.session_state["rebound_location"]:
             if selected == "Shot":
-                print("Shot", point)
                 st.session_state["shot_location"] = point
             elif selected == "Rebound":
-                print("Rebound", point)
                 st.session_state["rebound_location"] = point
             st.experimental_rerun()
 
@@ -79,7 +88,7 @@ if st.button('Add Row', type="primary"):
     else:
         st.error("Please select a shot and a rebound location")
 
-st.data_editor(data, width=1000)
+st.data_editor(data, width=1000, num_rows='dynamic')
 
 st.download_button(
     "⬇️ Download Data",
